c57771f34dff8f75e6a957145acf0455b4ac2484
Alyssa Hooper <nesteralyssa@gmail.com>
2020-10-23 18:20:05 -0500

    Adds Roomba Logic Skeleton

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {
 import {useDropzone} from 'react-dropzone'
 
 const style = {
-  margin: '10%',
+  margin: '10% 30% 10% 30%',
   flex: 1,
   display: 'flex',
   flexDirection: 'column',
@@ -27,12 +27,58 @@ function App() {
 
       reader.onabort = () => console.log('file reading was aborted')
       reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
+      reader.onload  = () => {
         const json = JSON.parse(reader.result)
         console.log(json)
+
+        const roombaRun = (json) => {
+          let initialRoombaLocation = json.initialRoombaLocation;
+          let dirtLocations         = json.dirtLocations;
+          let drivingInstructions   = json.drivingInstructions;
+          let x                     = json.roomDimensions[0];
+          let y                     = json.roomDimensions[1];
+          let i                     = 0;
+
+          for (i = initialRoombaLocation; i < drivingInstructions.length; i++) {
+
+
+            let count                   = 0;
+            let wallHitCount            = 0;
+            let wallHit                 = 'placeholder';
+            let roombaCurrentLocation   = 'x,y placeholder';
+
+            if (dirtLocations) {
+              dirtLocations++;
+            }
+
+            if (wallHit) {
+              wallHitCount++;
+            }
+
+            switch(json.drivingInstructions) {
+              case 'N':
+                y -= 1
+              break;
+              case 'S':
+                y += 1
+              break;
+              case 'E':
+                x += 1
+              break;
+              case 'W':
+                x -= 1
+              break;
+              default:
+            };
+        }
+
+        };
+
+        roombaRun(json)
       }
       reader.readAsText(file)
     })
+
 
   }, [])
   const {getRootProps, getInputProps} = useDropzone({onDrop})
